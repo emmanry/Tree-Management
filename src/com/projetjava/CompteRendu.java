7b15a3d45d1bb121c931a8d7654f6789bedaf984
emmanry <82103105+emmanry@users.noreply.github.com>
2021-05-09 15:19:44 +0100

    Add files via upload

--- src/com/projetjava/CompteRendu.java
+++ src/com/projetjava/CompteRendu.java
@@ -6,10 +6,14 @@ public class CompteRendu {
 
     private String rapport;
     private Date dateRapport;
+    private Association associationRapport;
+    private Membre membreRapport;
 
-    public CompteRendu(String contenu, int year, int month, int day){
+    public CompteRendu(String contenu, int year, int month, int day, Association association, Membre membre){
         this.rapport = contenu;
         this.dateRapport = new Date(year, month, day);
+        this.associationRapport = association;
+        this.membreRapport = membre;
     }
 
 }
