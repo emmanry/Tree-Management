52dbb77bbf38650e8a80b0dcd57fe64590367e5b
emmanry <emmanourry28@gmail.com>
2021-05-22 15:14:24 +0100

    maj Président

--- src/com/projetjava/President.java
+++ src/com/projetjava/President.java
@@ -4,15 +4,9 @@ import java.util.Date;
 
 public class President extends Membre{
 
-    private boolean president;
-
     public President(String lastname, String firstname, Association assoc, int yearNaissance, int monthNaissance, int dayNaissance, int yearInscription, int monthInscription, int dayInscription, String adresseMembre){
         super(lastname, firstname, assoc, yearNaissance, monthNaissance, dayNaissance, yearInscription, monthInscription, dayInscription, adresseMembre);
-        this.president = true;
-    }
 
-    public boolean estPresident(){
-        return this.president;
     }
 
 }
